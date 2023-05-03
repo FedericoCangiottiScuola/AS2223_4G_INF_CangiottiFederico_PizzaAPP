991388f9cd37cbe117699b7e6b6f44e8ce77ac4f
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-05-03 17:20:44 +0200

    feat: Add Articolo and Cliente model

---
+++ PizzaAPP/Models/Articolo.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaAPP.Models
+{
+    public class Articolo
+    {
+        public int Id { get; set; }
+        public string Descrizione { get; set; }
+        public float Prezzo { get; set; }
+        public string Valuta { get; set; }
+        public int Giacenza { get; set; }
+    }
+}
---
+++ PizzaAPP/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace PizzaAPP.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Citta { get; set; }
+        public string CodiceFiscale { get; set; }
+    }
+}
