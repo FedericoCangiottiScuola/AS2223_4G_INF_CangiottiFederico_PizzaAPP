2527c80c38ee288e8642333deee64f88b9aceca8
Federico Cangiotti <13657090-federicocangiotti@users.noreply.gitlab.com>
2023-05-03 17:35:14 +0200

    feat: Add init migration

---
+++ PizzaAPP/Data/Migrations/20230503152943_init.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaAPP.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articolo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descrizione = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Prezzo = table.Column<float>(type: "real", nullable: false),
+                    Valuta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Giacenza = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articolo", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cognome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Citta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodiceFiscale = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articolo");
+
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
